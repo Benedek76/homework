e664989bb4f83cbe3bb8ec1130243bef8ab61bab
Benedek76 <benedek.ladanyi@gmail.com>
2024-05-20 20:10:36 +0200

    Adding new routes for the test feature

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ABTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// AB Test.
+Route::post('/log-variant', [ABTestController::class, 'logVariant']);
+
+
+
+
+
